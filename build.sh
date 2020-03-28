84379d72a382b105f0f39a01353e3ed5749dafee
m <m@bitsnbites.eu>
2020-03-28 19:13:16 +0100

    build.sh: Add options for building individual components

--- build.sh
+++ build.sh
@@ -21,20 +21,68 @@
 #  3. This notice may not be removed or altered from any source distribution.
 ##############################################################################
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$SCRIPT_DIR"
+function help {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --help     Show this text"
+    echo "  --clean    Clean all the output folders"
+    echo "  --update   Update the Git submodules"
+    echo "  all        Build all components (default)"
+    echo "  binutils   Build only binutils"
+    echo "  bootstrap  Build only the bootstrap version of GCC (requires binutils)"
+    echo "  newlib     Build only newlib (requires bootstrap)"
+    echo "  gcc        Build only GCC (requires newlib)"
+}
 
 # Parse arguments.
 DO_CLEAN=no
 DO_UPDATE=no
+BUILD_BINUTILS=yes
+BUILD_BOOTSTRAP=yes
+BUILD_NEWLIB=yes
+BUILD_GCC=yes
 for arg in "$@" ; do
-    if [ "$arg" == "--clean" ] ; then
+    if [ "$arg" == "--help" ] ; then
+        help
+        exit 0
+    elif [ "$arg" == "--clean" ] ; then
         DO_CLEAN=yes
-    fi
-    if [ "$arg" == "--update" ] ; then
+    elif [ "$arg" == "--update" ] ; then
         DO_UPDATE=yes
+    elif [ "$arg" == "all" ] ; then
+        BUILD_BINUTILS=yes
+        BUILD_BOOTSTRAP=yes
+        BUILD_NEWLIB=yes
+        BUILD_GCC=yes
+    elif [ "$arg" == "binutils" ] ; then
+        BUILD_BINUTILS=yes
+        BUILD_BOOTSTRAP=no
+        BUILD_NEWLIB=no
+        BUILD_GCC=no
+    elif [ "$arg" == "bootstrap" ] ; then
+        BUILD_BINUTILS=no
+        BUILD_BOOTSTRAP=yes
+        BUILD_NEWLIB=no
+        BUILD_GCC=no
+    elif [ "$arg" == "newlib" ] ; then
+        BUILD_BINUTILS=no
+        BUILD_BOOTSTRAP=no
+        BUILD_NEWLIB=yes
+        BUILD_GCC=no
+    elif [ "$arg" == "gcc" ] ; then
+        BUILD_BINUTILS=no
+        BUILD_BOOTSTRAP=no
+        BUILD_NEWLIB=no
+        BUILD_GCC=yes
+    else
+        echo "*** Invalid argument: $arg"
+        help
+        exit 1
     fi
 done
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIR"
 
 # Check dependencies.
 # TODO(m): Implement me!
@@ -65,63 +113,72 @@ TARGET=mrisc32-elf
 set -e
 
 # Build binutils.
-echo "====> Building binutils"
-mkdir -p out/binutils
-cd out/binutils
-../../ext/binutils-mrisc32/configure \
-    --prefix="$INSTALL_ROOT" \
-    --target="$TARGET" \
-    --with-system-zlib \
-    --disable-gdb \
-    --disable-sim \
-    > configure.log 2>&1
-make all > build.log 2>&1
-make install > install.log 2>&1
-cd ../..
+if [ "$BUILD_BINUTILS" == "yes" ] ; then
+    echo "====> Building binutils"
+    mkdir -p out/binutils
+    cd out/binutils
+    ../../ext/binutils-mrisc32/configure \
+        --prefix="$INSTALL_ROOT" \
+        --target="$TARGET" \
+        --with-system-zlib \
+        --disable-gdb \
+        --disable-sim \
+        > configure.log 2>&1
+    make all > build.log 2>&1
+    make install > install.log 2>&1
+    cd ../..
+fi
 
 # Build bootstrap gcc.
-echo "====> Building bootstrap GCC"
-mkdir -p out/gcc-bootstrap
-cd out/gcc-bootstrap
-../../ext/gcc-mrisc32/configure \
-  --prefix="$INSTALL_ROOT" \
-  --target="$TARGET" \
-  --enable-languages=c \
-  --without-headers \
-  --with-newlib \
-  --with-gnu-as \
-  --with-gnu-ld \
-  > configure.log 2>&1
-make -j20 all-gcc > build.log 2>&1
-make install-gcc > install.log 2>&1
-cd ../..
+if [ "$BUILD_BOOTSTRAP" == "yes" ] ; then
+    echo "====> Building bootstrap GCC"
+    mkdir -p out/gcc-bootstrap
+    cd out/gcc-bootstrap
+    ../../ext/gcc-mrisc32/configure \
+      --prefix="$INSTALL_ROOT" \
+      --target="$TARGET" \
+      --enable-languages=c \
+      --without-headers \
+      --with-newlib \
+      --with-gnu-as \
+      --with-gnu-ld \
+      > configure.log 2>&1
+    make -j20 all-gcc > build.log 2>&1
+    make install-gcc > install.log 2>&1
+    cd ../..
+fi
 
 # Build newlib.
-echo "====> Building newlib"
-mkdir -p out/newlib
-cd out/newlib
-../../ext/newlib-mrisc32/configure \
-  --prefix="$INSTALL_ROOT" \
-  --target="$TARGET" \
-  > configure.log 2>&1
-make -j20 all > build.log 2>&1
-make install > install.log 2>&1
-cd ../..
+if [ "$BUILD_NEWLIB" == "yes" ] ; then
+    echo "====> Building newlib"
+    mkdir -p out/newlib
+    cd out/newlib
+    ../../ext/newlib-mrisc32/configure \
+      --prefix="$INSTALL_ROOT" \
+      --target="$TARGET" \
+      > configure.log 2>&1
+    make -j20 all > build.log 2>&1
+    make install > install.log 2>&1
+    cd ../..
+fi
 
 # Build gcc with newlib.
-echo "====> Building GCC"
-mkdir -p out/gcc
-cd out/gcc
-../../ext/gcc-mrisc32/configure \
-  --prefix="$INSTALL_ROOT" \
-  --target="$TARGET" \
-  --enable-languages=c \
-  --with-newlib \
-  --with-gnu-as \
-  --with-gnu-ld \
-  --disable-shared \
-  --disable-libssp \
-  > configure.log 2>&1
-make -j20 all > build.log 2>&1
-make install > install.log 2>&1
+if [ "$BUILD_GCC" == "yes" ] ; then
+    echo "====> Building GCC"
+    mkdir -p out/gcc
+    cd out/gcc
+    ../../ext/gcc-mrisc32/configure \
+      --prefix="$INSTALL_ROOT" \
+      --target="$TARGET" \
+      --enable-languages=c \
+      --with-newlib \
+      --with-gnu-as \
+      --with-gnu-ld \
+      --disable-shared \
+      --disable-libssp \
+      > configure.log 2>&1
+    make -j20 all > build.log 2>&1
+    make install > install.log 2>&1
+    cd ../..
+fi
 
